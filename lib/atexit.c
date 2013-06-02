fb9cd6d6128c12c3d6250219bee2825d0d5dcac6
He Chunhui <hchunhui@mail.ustc.edu.cn>
2013-06-02 20:14:31 +0800

    __cxa_atexit

--- lib/atexit.c
+++ lib/atexit.c
@@ -1,31 +1,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef void (*function)(void);
+int __cxa_atexit(void (*f)(void *), void *p, void *d);
+void __cxa_finalize(void *d);
 
-#define NUM_ATEXIT	32
+struct object
+{
+	void (*f)(void*);
+	void *p;
+	void *d;
+} object[64];
+unsigned int __cxa_atexit_count = 0;
 
-static function __atexitlist[NUM_ATEXIT];
-static int atexit_counter;
+int __cxa_atexit(void (*f)(void *), void *p, void *d)
+{
+	if (__cxa_atexit_count >= 64) return -1;
+	object[__cxa_atexit_count].f = f;
+	object[__cxa_atexit_count].p = p;
+	object[__cxa_atexit_count].d = d;
+	++__cxa_atexit_count;
+	return 0;
+}
 
-int atexit(function t) {
-  if (atexit_counter<NUM_ATEXIT) {
-    __atexitlist[atexit_counter]=t;
-    ++atexit_counter;
-    return 0;
-  }
-  return -1;
+/* This currently destroys all objects */
+void __cxa_finalize(void *d)
+{
+	unsigned int i = __cxa_atexit_count;
+	if(d)
+	{
+		return;
+	}
+	for (; i > 0; --i)
+	{
+		--__cxa_atexit_count;
+		object[__cxa_atexit_count].f(object[__cxa_atexit_count].p);
+        }
+}
+
+int atexit(void (*t)()) {
+  return __cxa_atexit(t, NULL, NULL);
 }
 
 extern void __thread_doexit(int doexit);
 
 void __libc_exit(int code);
 void __libc_exit(int code) {
-  register int i=atexit_counter;
   __thread_doexit(code);
-  while(i) {
-    __atexitlist[--i]();
-  }
+  __cxa_finalize(NULL);
   _exit(code);
 }
 void exit(int code) __attribute__((alias("__libc_exit")));
