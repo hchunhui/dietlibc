16fe119db9ffb156941607ca4b82122bfc16d8d1
He Chunhui <hchunhui@mail.ustc.edu.cn>
2015-03-15 19:47:45 +0800

    workaround

--- syscalls.c/getcwd.c
+++ syscalls.c/getcwd.c
@@ -1,9 +1,13 @@
 #include <sys/types.h>
+#include <errno.h>
 
-char *__syscall_getcwd(char *buf, size_t size)
+int __syscall_getcwd(char *buf, size_t size)
 {
-	buf[0] = '/';
-	buf[1] = 0;
-	return buf;
+	if(buf && size) {
+		buf[0] = '/';
+		buf[1] = 0;
+		return 1;
+	}
+	return -EINVAL;
 }
 
--- syscalls.c/gettimeofday.c
+++ syscalls.c/gettimeofday.c
@@ -3,8 +3,14 @@
 long __myos_gettime();
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
-	tv->tv_sec = __myos_gettime();
-	tv->tv_usec = 0;
+	if(tv) {
+		tv->tv_sec = __myos_gettime();
+		tv->tv_usec = 0;
+	}
+	if(tz) {
+		tz->tz_minuteswest = 0;
+		tz->tz_dsttime = 0;
+	}
 	return 0;
 }
 
